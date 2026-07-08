59ce1068c05062f017f6b2a8efe9a49382b473f0
ilevyzaga <i.levy.zaga@hotmail.com>
2026-07-08 09:52:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -28,11 +28,15 @@ tarjeta.innerHTML = `
 <p><b>🛡️ Defensa</b></p>
 <p>América: 0.9 goles recibidos</p>
 
+<p><b>🏟️ Factor clave</b></p>
+<p>La localía puede marcar diferencia.</p>
+
 <p><b>🤖 Predicción MatchIQ</b></p>
 <p>América gana o empate</p>
 
 <p>📈 Confianza: 80%</p>
 `;
+
 }
 
 
@@ -52,11 +56,15 @@ tarjeta.innerHTML = `
 <p><b>🛡️ Defensa</b></p>
 <p>Tigres tiene mayor equilibrio defensivo.</p>
 
+<p><b>🏟️ Factor clave</b></p>
+<p>El estadio Universitario favorece a Tigres.</p>
+
 <p><b>🤖 Predicción MatchIQ</b></p>
 <p>Tigres gana o empate</p>
 
 <p>📈 Confianza: 75%</p>
 `;
+
 }
 
 
@@ -71,23 +79,23 @@ tarjeta.innerHTML = `
 <p>Monterrey: 🟢 G 🟢 G 🟡 E 🟡 E 🔴 P</p>
 
 <p><b>⚽ Ataque</b></p>
-<p>Partido con ataque equilibrado.</p>
+<p>Ambos equipos tienen jugadores ofensivos importantes.</p>
 
 <p><b>🛡️ Defensa</b></p>
-<p>Ambos equipos pueden cerrar espacios.</p>
+<p>Puede ser un partido cerrado.</p>
 
 <p><b>🤖 Predicción MatchIQ</b></p>
-<p>Partido cerrado</p>
+<p>Partido equilibrado</p>
 
 <p>📈 Confianza: 65%</p>
 `;
+
 }
 
 
 boton.parentElement.appendChild(tarjeta);
 
 }
-
 
 
 
@@ -100,12 +108,17 @@ let contenido = document.getElementById("contenido");
 if(seccion === "inicio"){
 
 contenido.innerHTML = `
+
 <h2>Liga MX - Jornada 1</h2>
 
 <div class="card">
+
 <h2>⚽ Partidos de hoy</h2>
-<p>Revisa los encuentros y predicciones inteligentes.</p>
+
+<p>Revisa los partidos y predicciones inteligentes.</p>
+
 </div>
+
 `;
 
 }
@@ -115,14 +128,21 @@ contenido.innerHTML = `
 if(seccion === "estadisticas"){
 
 contenido.innerHTML = `
+
 <h2>📊 Estadísticas</h2>
 
 <div class="card">
+
 <p>Próximamente:</p>
-<p>• Goles por partido</p>
-<p>• Rendimiento ofensivo</p>
-<p>• Defensa</p>
+
+<p>⚽ Goles por partido</p>
+
+<p>🛡️ Rendimiento defensivo</p>
+
+<p>📈 Comparación de equipos</p>
+
 </div>
+
 `;
 
 }
@@ -132,12 +152,17 @@ contenido.innerHTML = `
 if(seccion === "predicciones"){
 
 contenido.innerHTML = `
+
 <h2>🤖 Predicciones</h2>
 
 <div class="card">
-<p>Modelo MatchIQ analizando partidos.</p>
+
+<p>El modelo MatchIQ analizará datos históricos.</p>
+
 <p>Predicciones inteligentes próximamente.</p>
+
 </div>
+
 `;
 
 }
@@ -147,15 +172,19 @@ contenido.innerHTML = `
 if(seccion === "perfil"){
 
 contenido.innerHTML = `
+
 <h2>⚙️ Perfil</h2>
 
 <div class="card">
+
 <p>Usuario MatchIQ MX</p>
+
 <p>Configuración próximamente.</p>
+
 </div>
+
 `;
 
 }
-
 
 }
