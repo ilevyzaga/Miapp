b3b64cd544dbf3f1e96f6fffa8eedae8e7499c75
ilevyzaga <i.levy.zaga@hotmail.com>
2026-07-08 09:53:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -28,9 +28,6 @@ tarjeta.innerHTML = `
 <p><b>🛡️ Defensa</b></p>
 <p>América: 0.9 goles recibidos</p>
 
-<p><b>🏟️ Factor clave</b></p>
-<p>La localía puede marcar diferencia.</p>
-
 <p><b>🤖 Predicción MatchIQ</b></p>
 <p>América gana o empate</p>
 
@@ -38,7 +35,6 @@ tarjeta.innerHTML = `
 `;
 
 }
-
 
 
 if(equipo === "tigres"){
@@ -56,9 +52,6 @@ tarjeta.innerHTML = `
 <p><b>🛡️ Defensa</b></p>
 <p>Tigres tiene mayor equilibrio defensivo.</p>
 
-<p><b>🏟️ Factor clave</b></p>
-<p>El estadio Universitario favorece a Tigres.</p>
-
 <p><b>🤖 Predicción MatchIQ</b></p>
 <p>Tigres gana o empate</p>
 
@@ -66,7 +59,6 @@ tarjeta.innerHTML = `
 `;
 
 }
-
 
 
 if(equipo === "cruzazul"){
@@ -79,7 +71,7 @@ tarjeta.innerHTML = `
 <p>Monterrey: 🟢 G 🟢 G 🟡 E 🟡 E 🔴 P</p>
 
 <p><b>⚽ Ataque</b></p>
-<p>Ambos equipos tienen jugadores ofensivos importantes.</p>
+<p>Partido equilibrado ofensivamente.</p>
 
 <p><b>🛡️ Defensa</b></p>
 <p>Puede ser un partido cerrado.</p>
@@ -96,6 +88,7 @@ tarjeta.innerHTML = `
 boton.parentElement.appendChild(tarjeta);
 
 }
+
 
 
 
@@ -129,17 +122,46 @@ if(seccion === "estadisticas"){
 
 contenido.innerHTML = `
 
-<h2>📊 Estadísticas</h2>
+<h2>📊 Estadísticas Liga MX</h2>
+
 
 <div class="card">
 
-<p>Próximamente:</p>
+<h3>🏆 Tabla de posiciones</h3>
 
-<p>⚽ Goles por partido</p>
+<p>1. 🦅 América ........ 15 pts</p>
 
-<p>🛡️ Rendimiento defensivo</p>
+<p>2. 🐯 Tigres ........ 13 pts</p>
 
-<p>📈 Comparación de equipos</p>
+<p>3. 🔵 Monterrey ..... 12 pts</p>
+
+<p>4. 🐐 Chivas ........ 10 pts</p>
+
+</div>
+
+
+<div class="card">
+
+<h3>⚽ Goles por partido</h3>
+
+<p>🦅 América: 1.8 goles</p>
+
+<p>🐯 Tigres: 1.6 goles</p>
+
+<p>🔵 Monterrey: 1.5 goles</p>
+
+<p>🐐 Chivas: 1.2 goles</p>
+
+</div>
+
+
+<div class="card">
+
+<h3>🛡️ Mejor defensa</h3>
+
+<p>🦅 América: 0.9 goles recibidos</p>
+
+<p>🐯 Tigres: 1.0 goles recibidos</p>
 
 </div>
 
@@ -157,9 +179,7 @@ contenido.innerHTML = `
 
 <div class="card">
 
-<p>El modelo MatchIQ analizará datos históricos.</p>
-
-<p>Predicciones inteligentes próximamente.</p>
+<p>El modelo MatchIQ analiza datos históricos para generar predicciones.</p>
 
 </div>
 
@@ -179,12 +199,13 @@ contenido.innerHTML = `
 
 <p>Usuario MatchIQ MX</p>
 
-<p>Configuración próximamente.</p>
+<p>Preferencias próximamente.</p>
 
 </div>
 
 `;
 
 }
+
 
 }
