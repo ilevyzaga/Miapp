b47eb4b83ed007f296b3468df5c76fabd96c9e19
ilevyzaga <i.levy.zaga@hotmail.com>
2026-07-10 22:04:00 -0600

    Update football.js

--- api/football.js
+++ api/football.js
@@ -2,12 +2,19 @@ export default async function handler(req, res) {
 
 
 const response = await fetch(
-"https://v3.football.api-sports.io/status",
+
+"https://v3.football.api-sports.io/teams?league=262&season=2026",
+
 {
+
 headers:{
+
 "x-apisports-key": process.env.API_KEY
+
 }
+
 }
+
 );
 
 
