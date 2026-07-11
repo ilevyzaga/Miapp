9b00092366fc204811e747bd8377b50b4f50c811
ilevyzaga <i.levy.zaga@hotmail.com>
2026-07-11 00:32:33 -0600

    Update sportsdb.js

--- api/sportsdb.js
+++ api/sportsdb.js
@@ -1,68 +1,48 @@
-export default async function handler(req, res){
+export default async function handler(req, res) {
 
-const API_KEY = "3";
+  const API_KEY = "3";
+  const league = "4350";
 
-const type = req.query.type || "events";
+  const type = req.query.type || "fixtures";
 
-let url = "";
+  let url = "";
 
+  if(type === "teams") {
 
-// Liga MX
-const league = "4350";
+    url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/lookup_all_teams.php?id=${league}`;
 
-if(type === "teams"){
+  } else if(type === "fixtures") {
 
-url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/lookup_all_teams.php?id=${league}`;
+    url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventsnextleague.php?id=${league}`;
 
-}
+  } else if(type === "past") {
 
+    url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventspastleague.php?id=${league}`;
 
-else if(type === "fixtures"){
+  } else {
 
-url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventsnextleague.php?id=${league}`;
+    return res.status(400).json({
+      error: "Tipo no válido"
+    });
 
-}
-
-
-else if(type === "past"){
-
-url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventspastleague.php?id=${league}`;
-
-}
+  }
 
 
-else{
+  try {
 
-url = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventsnextleague.php?id=${league}`;
+    const respuesta = await fetch(url);
 
-}
+    const datos = await respuesta.json();
 
-
-
-try{
+    res.status(200).json(datos);
 
 
-const respuesta = await fetch(url);
+  } catch(error) {
 
-const datos = await respuesta.json();
+    res.status(500).json({
+      error: error.message
+    });
 
-
-res.status(200).json(datos);
-
-
-}
-
-catch(error){
-
-
-res.status(500).json({
-
-error:error.message
-
-});
-
-
-}
-
+  }
 
 }
